824a026f1679bb76dea7e97a722883dc357a8852
Haru <67126370+Harull@users.noreply.github.com>
2023-06-01 17:43:18 +0200

    Add files via upload
    RAJOUT DES HITBOX POUR CETAINES PARTIES DES DECORS

--- Godot-main/JeuxGodot/Joueur.gd
+++ Godot-main/JeuxGodot/Joueur.gd
@@ -25,6 +25,7 @@ const VITESSE = 500
 const SAUT = -500
 const GRAVITE = 20
 const DECELERATION = 20
+var lolhesfalling = 0
 
 
 
@@ -55,13 +56,26 @@ func _physics_process(_delta):
 		_animated_sprite.set_flip_h(false)
 		
 	#DIFFERENTES ANIMATIONS DES ACTIONS	
-	if Input.is_action_just_pressed("ui_up"):
+	
+	if lolhesfalling == 0 and Input.is_action_just_pressed("ui_up"):
 		_animated_sprite.play("jumping")
+		lolhesfalling += 1
+	elif not is_on_floor() and lolhesfalling == 0:
+		_animated_sprite.play("falling")
 	elif is_on_floor():
+		lolhesfalling = 0
 		if Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left"):
 			_animated_sprite.play("running")
+		elif Input.is_action_pressed("ui_down"):
+			_animated_sprite.play("crouching")
 		else:
-			_animated_sprite.play("standing")	
+			_animated_sprite.play("standing")
+	if not is_on_floor():
+		if lolhesfalling == 60:
+			_animated_sprite.play("falling")
+		lolhesfalling += 1
+			
+	
 	move_and_slide()
 	
 	
